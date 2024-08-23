7cf268c47d63d601d40622664ddf2b5d9954179f
Enzo Negrão Freitas <enzo_negrao_freitas@outlook.com>
2024-08-23 20:51:44 -0300

    Fiz a Interface

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowTipButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetHighTipButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
