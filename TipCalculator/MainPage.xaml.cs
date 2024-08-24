bfa18b9e944df6f4855f524e925efb02dadbc014
Enzo Negrão Freitas <enzo_negrao_freitas@outlook.com>
2024-08-23 22:44:07 -0300

    Adicionei a lógica do cálculo da gorjeta

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+        double Bill = 0, Tip = 0, Total = 0, TipPercent = 0.15;
 
         public MainPage()
         {
@@ -10,22 +11,41 @@
 
         private void SetLowTipButton_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 15;
         }
 
         private void SetHighTipButton_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 20;
         }
 
         private void ButtonRoundDown_Clicked(object sender, EventArgs e)
         {
-
+            
         }
 
         private void ButtonRoundUp_Clicked(object sender, EventArgs e)
         {
+           
+        }
 
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            LabelPercent.Text = TipSlider.Value.ToString("#.##") + "%";
+            TipPercent = TipSlider.Value / 100;
+            Tip = Bill * TipPercent;
+            Total = Bill + Tip;
+            TipLabel.Text = Tip.ToString("C");
+            TotalLabel.Text = Total.ToString("C");
+        }
+
+        private void EntryAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Bill = float.Parse(EntryAmount.Text);
+            Tip = Bill * TipPercent;
+            Total = Bill + Tip;
+            TipLabel.Text = Tip.ToString("C");
+            TotalLabel.Text = Total.ToString("C");
         }
     }
 
